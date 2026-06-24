98313f56f9b9b092be318bb51727db4e09e74649
Gabriel Lavor <contato.devlavor@outlook.com>
2026-06-24 18:43:27 -0300

    meta sobe 50 pontos fixos ao ser atingida

--- core/views.py
+++ core/views.py
@@ -127,8 +127,7 @@ def tarefas_view(request):
     meta_atingida = False
     if f.pontos >= f.meta_mensal and not request.session.get(f'meta_parabens_{f.id}'):
         meta_atingida = True
-        # Sobe a meta em 50%
-        f.meta_mensal = int(f.meta_mensal * 1.5)
+        f.meta_mensal = f.meta_mensal + 50
         f.save()
         # Marca na sessão para não mostrar de novo
         request.session[f'meta_parabens_{f.id}'] = True
